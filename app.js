47360ea716d5179ebf970beb0ad5db449f2ff9e8
Rehan Kaleem <rehk@bgs.ac.uk>
2019-05-28 16:48:39 +0100

    Added sample test image

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 var Jimp = require("jimp2");
  
 // open a file called "lenna.png"
-Jimp.read("test.jpg", function (err, image) {
+Jimp.read("test.png", function (err, image) {
     if (err) throw err;
-    image.resize(256, Jimp.AUTO)            // resize
-         .quality(60)                 // set JPEG quality
-         .greyscale()                 // set greyscale
-         .write("test_r.jpg"); // save
+    image.greyscale();                 // set greyscale
+         //.write("test_r.jpg"); // save
     console.log (image.bitmap.width);
     console.log (image.bitmap.height);
 });
 
-Jimp.read ("test.jpg", function (err, image){
+Jimp.read ("test.png", function (err, image){
 	image.greyscale();
-	var testPixel = image.getPixelColor(818, 261);
+	var testPixel = image.getPixelColor(1, 1);
 	var testPixel = Jimp.intToRGBA(testPixel);
 	console.log(testPixel);
 });
