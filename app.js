aa1c1cfb0a028ee00b2c0378e0522f6258d46b05
Rehan Kaleem <rehk@bgs.ac.uk>
2019-05-29 16:40:53 +0100

    Added dimensions scaling 3pixels

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var Jimp = require("jimp2");
  
-// open a file called "lenna.png"
 Jimp.read("test.png", function (err, image) {
     if (err) throw err;
     image.greyscale();                 // set greyscale
          //.write("test_r.jpg"); // save
-    console.log (image.bitmap.width);
-    console.log (image.bitmap.height);
+    var imageW = image.bitmap.width;
+    var imageH = image.bitmap.height;
+    var crossW = Math.floor(imageW / 3);
+    var crossH = Math.floor(imageH / 3);
+    var crossW = crossW * 3;
+    var crossH = crossH * 3;
+    var crossW = crossW - 1;
+    var crossH = crossH - 1;
+    console.log(imageW);
+    console.log(imageH);
+    console.log(crossW);
+    console.log(crossH);
 });
 
 Jimp.read ("test.png", function (err, image){
